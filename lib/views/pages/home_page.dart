6d5c00f1edfb6da8110caff1efc3c5a4ca6918dd
Joseph Backelant's <backelantsjoseph@gmail.com>
2024-03-19 11:23:57 +0100

    modfication interface home page

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite_test/models/item_list.dart';
 import 'package:sqflite_test/services/database_client.dart';
+import 'package:sqflite_test/views/Widgets/add_dialog.dart';
 import 'package:sqflite_test/views/Widgets/custom_appbar.dart';
+import 'package:sqflite_test/views/Widgets/item_list_tile.dart';
+import 'package:sqflite_test/views/pages/article_listview.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -11,7 +14,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  List<ItemList> item = [];
+  List<ItemList> items = [];
 
   @override
   void initState() {
@@ -21,20 +24,63 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(
-          buttonTitle: "Ajouter",
-          callback: addItemList,
-          titleString: "Ma liste de souhait"),
-      body: Center(child: Text("nous avons ${item.length} elements")),
-    );
+        appBar: CustomAppbar(
+            buttonTitle: "Ajouter",
+            callback: addItemList,
+            titleString: "Ma liste de souhait"),
+        body: ListView.separated(
+          itemBuilder: ((context, index) {
+            final item = items[index];
+            return ItemListWidget(
+                itemList: item,
+                onPressed: onListPressed,
+                onDeleted: onDeleteItem);
+          }),
+          separatorBuilder: ((context, index) => const Divider()),
+          itemCount: items.length,
+        ));
   }
 
   getItemList() async {
     final fromDb = await DatabaseClient().allItem();
     setState(() {
-      item = fromDb;
+      items = fromDb;
     });
   }
 
-  addItemList() {}
+  void addItemList() async {
+    final controller = TextEditingController();
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AddDialog(
+              controller: controller,
+              onAdded: (() {
+                handleCloseDialog();
+                if (controller.text.isEmpty) return;
+                DatabaseClient()
+                    .addItemList(controller.text)
+                    .then((success) => getItemList());
+              }),
+              onCancel: handleCloseDialog);
+        });
+
+    print('enregistrement done');
+  }
+
+  handleCloseDialog() {
+    Navigator.pop(context);
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  onListPressed(ItemList itemList) {
+    final next = ArticleListView(itemList: itemList);
+    MaterialPageRoute materialPageRoute =
+        MaterialPageRoute(builder: (context) => next);
+    Navigator.of(context).push(materialPageRoute);
+  }
+
+  onDeleteItem(ItemList itemList) {
+    DatabaseClient().removeItem(itemList).then((sucess) => getItemList());
+  }
 }
